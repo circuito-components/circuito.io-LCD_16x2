7420fd0f644454e8d613350f1ad9f47669ccfd3a
Ohad Rusnak <ohad@robo-plan.com>
2017-04-26 12:26:46 +0000

    firmware update

--- Arduino/Firmware.ino
+++ Arduino/Firmware.ino
@@ -1,11 +1,22 @@
-// Include Libraries
+// // ***************************** Include Libraries *************************************
 #include "Arduino.h"
+// ****************************************************************************************
 
 // Pin Definitions
+#define LCD_PIN_RS	2
+#define LCD_PIN_R_W	9
+#define LCD_PIN_E	4
+#define LCD_PIN_DB4	8
+#define LCD_PIN_DB5	6
+#define LCD_PIN_DB6	7
+#define LCD_PIN_DB7	3
 
-// Global variables and defines
+// ***************************** Global variables and defines *****************************
 
-// Constructors
+// ****************************************************************************************
+// ******************************** object initialization *********************************
+
+// ****************************************************************************************
 
 /* This code sets up the essentials for your circuit to work. It runs first every time your circuit is powered with electricity. */
 void setup() {
@@ -15,12 +26,15 @@ void setup() {
     while (!Serial) ; // wait for serial port to connect. Needed for native USB
     Serial.println("start");
     
-    //WaterFlow - setup
-    
+    // ******************************* LCD16X2 - setup *************************************
+
+    // *************************************************************************************
 }
 
 /* This code is the main logic of your circuit. It defines the interaction between the components you selected. After setup, it runs over and over again, in an eternal loop. */
 void loop() {
-    //WaterFlow - Test Code
+    // ******************************* LCD16X2 - Test Code *********************************
+   
+    // *************************************************************************************
     
 }
